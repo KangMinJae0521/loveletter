466402b898acead3d6e9afe3c7d20c7bef3a0e7d
KangMinJae0521 <61584201+KangMinJae0521@users.noreply.github.com>
2024-05-31 17:15:55 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,95 @@
+import streamlit as st
+import time
 
+class LoveAdviceApp:
+    def __init__(self):
+        self.user_info = {}
+        self.options = {
+            "age": ["20대", "30대", "40대 이상"],
+            "interest": ["영화", "책", "운동"]
+        }
+        self.current_question = 0
+
+    def set_user_info(self, info_type, option):
+        self.user_info[info_type] = option
+
+    def show_advice(self):
+        age = self.user_info.get("age", "미정")
+        interest = self.user_info.get("interest", "미정")
+        advice = f"당신은 {age}이고, 관심사는 {interest}입니다. 사랑은 언제나 가까이에 있습니다."
+        st.info(advice)
+
+    def run(self):
+        if "advice_requested" not in st.session_state:
+            st.session_state.advice_requested = False
+        if "dialogue_shown" not in st.session_state:
+            st.session_state.dialogue_shown = False
+        if "current_question" not in st.session_state:
+            st.session_state.current_question = 0
+
+        if st.session_state.dialogue_shown == False:
+            st.write('나: "하...연애하고 싶다...ㅠㅠ"')
+            time.sleep(3)
+            st.write('나: "누가 연애 조언 안해주나...?"')
+            time.sleep(3)
+            st.write('슝슝이: "안녕? 난 슝슝이야!"')
+            time.sleep(3)
+            st.write('슝슝이: "내가 너의 모솔 탈출을 도와줄게!"')
+            time.sleep(3)
+            st.write('슝슝이: "일단 내가 만든 질문에 답을 해봐!"')
+            time.sleep(3)
+            st.session_state.dialogue_shown = True
+
+        if st.session_state.dialogue_shown and not st.session_state.advice_requested:
+            if st.button("연애 조언 받기"):
+                st.session_state.advice_requested = True
+                st.experimental_rerun()
+
+        if st.session_state.advice_requested:
+            if st.session_state.current_question < len(self.options):
+                info_type = list(self.options.keys())[st.session_state.current_question]
+                options = self.options[info_type]
+                choice = st.radio(f"당신의 {info_type}을 선택하세요:", options)
+                if st.button("다음"):
+                    self.set_user_info(info_type, choice)
+                    st.session_state.current_question += 1
+                    st.experimental_rerun()
+            else:
+                self.show_advice()
+
+            if st.button("초기화"):
+                self.user_info = {}
+                st.session_state.advice_requested = False
+                st.session_state.dialogue_shown = False
+                st.session_state.current_question = 0
+                st.experimental_rerun()
+
+def add_bg_from_local():
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url("https://cdn.banggooso.com/assets/images/uploadImg/1596642366(M).jpg");
+            background-size: 50%;
+            background-position: center top;
+            background-repeat: no-repeat;
+            background-color: white;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+def main():
+    # 앱의 타이틀 설정
+    st.title("사랑을 찾아 슝슝~♥")
+
+    # 배경 이미지 추가
+    add_bg_from_local()
+
+    # 연애 조언 앱 실행
+    app = LoveAdviceApp()
+    app.run()
+
+if __name__ == "__main__":
+    main()
