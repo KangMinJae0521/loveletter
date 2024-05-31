eedd4a9aef706bbfc80beb832e13be2593858233
KangMinJae0521 <61584201+KangMinJae0521@users.noreply.github.com>
2024-05-31 13:26:42 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,20 +18,26 @@ class LoveAdviceApp:
         st.info(advice)
 
     def run(self):
+        
 
-        # 사용자 정보 선택
-        for info_type, options in self.options.items():
-            choice = st.radio(f"당신의 {info_type}을 선택하세요:", options)
-            self.set_user_info(info_type, choice)
+        if "advice_requested" not in st.session_state:
+            st.session_state.advice_requested = False
 
-        # 조언 받기 버튼
-        if st.button("조언 받기"):
-            self.show_advice()
+        if st.button("연애 조언 받기"):
+            st.session_state.advice_requested = True
 
-        # 초기화 체크박스
-        if st.button("초기화"):
-            self.user_info = {}
-            st.experimental_rerun()
+        if st.session_state.advice_requested:
+            for info_type, options in self.options.items():
+                choice = st.radio(f"당신의 {info_type}을 선택하세요:", options)
+                self.set_user_info(info_type, choice)
+
+            if st.button("조언 받기"):
+                self.show_advice()
+
+            if st.button("초기화"):
+                self.user_info = {}
+                st.session_state.advice_requested = False
+                st.experimental_rerun()
 
 def main():
     # 앱의 타이틀 설정
@@ -42,10 +48,6 @@ def main():
     st.write('나: "누가 연재 조언 안해주나...?"')
     st.write('슝슝이: "안녕? 난 슝슝이야!"')
     st.write('나: "내가 너의 모솔 탈출을 도와줄게!"')
-
-    if st.button("연애 조언 받기"):
-        app = LoveAdviceApp()
-        app.run()
 
     # 연애 조언 앱 실행
     app = LoveAdviceApp()
