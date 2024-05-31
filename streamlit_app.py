0fd32716bd07970bf007ad9e033390f35ff2404b
KangMinJae0521 <61584201+KangMinJae0521@users.noreply.github.com>
2024-05-31 13:20:47 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,16 +18,18 @@ class LoveAdviceApp:
         st.info(advice)
 
     def run(self):
-        st.title("연애 조언 앱")
 
+        # 사용자 정보 선택
         for info_type, options in self.options.items():
             choice = st.radio(f"당신의 {info_type}을 선택하세요:", options)
             self.set_user_info(info_type, choice)
 
+        # 조언 받기 버튼
         if st.button("조언 받기"):
             self.show_advice()
 
-        if st.button("초기화"):
+        # 초기화 체크박스
+        if st.checkbox("초기화"):
             self.user_info = {}
             st.experimental_rerun()
 
@@ -41,11 +43,9 @@ def main():
     st.write('슝슝이: "안녕? 난 슝슝이야!"')
     st.write('나: "내가 너의 모솔 탈출을 도와줄게!"')
 
-    # '연애 조언 받기' 버튼 생성
-    if st.button("연애 조언 받기"):
-        app = LoveAdviceApp()
-        app.run()
+    # 연애 조언 앱 실행
+    app = LoveAdviceApp()
+    app.run()
 
 if __name__ == "__main__":
     main()
-
