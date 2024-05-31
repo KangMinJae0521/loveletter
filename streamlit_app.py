e5bc4068efd69d6127ef0d567ef37e79b76e0fc3
KangMinJae0521 <61584201+KangMinJae0521@users.noreply.github.com>
2024-05-31 14:22:09 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,26 +21,39 @@ class LoveAdviceApp:
     def run(self):
         if "advice_requested" not in st.session_state:
             st.session_state.advice_requested = False
+        if "dialogue_shown" not in st.session_state:
+            st.session_state.dialogue_shown = False
+
+        if st.session_state.dialogue_shown == False:
+            # 대사 표시
+            st.write('나: "하...연애하고 싶다...ㅠㅠ"')
+            time.sleep(3)
+            st.write('나: "누가 연애 조언 안해주나...?"')
+            time.sleep(3)
+            st.write('슝슝이: "안녕? 난 슝슝이야!"')
+            time.sleep(3)
+            st.write('슝슝이: "내가 너의 모솔 탈출을 도와줄게!"')
+            time.sleep(3)
+            st.write('슝슝이: "일단 내가 만든 질문에 답을 해봐!"')
+            time.sleep(3)
+            st.session_state.dialogue_shown = True
+
+        if st.button("연애 조언 받기"):
+            st.session_state.advice_requested = True
 
         if st.session_state.advice_requested:
-            self.collect_user_info()
-        else:
-            if st.button("연애 조언 받기"):
-                st.session_state.advice_requested = True
-                self.collect_user_info()
+            for info_type, options in self.options.items():
+                choice = st.radio(f"당신의 {info_type}을 선택하세요:", options)
+                self.set_user_info(info_type, choice)
 
-    def collect_user_info(self):
-        for info_type, options in self.options.items():
-            choice = st.radio(f"당신의 {info_type}을 선택하세요:", options, key=info_type)
-            self.set_user_info(info_type, choice)
- 
-        if st.button("조언 받기"):
-            self.show_advice()
+            if st.button("조언 받기"):
+                self.show_advice()
 
-        if st.button("초기화"):
-            self.user_info = {}
-            st.session_state.advice_requested = False
-            st.experimental_rerun()
+            if st.button("초기화"):
+                self.user_info = {}
+                st.session_state.advice_requested = False
+                st.session_state.dialogue_shown = False
+                st.experimental_rerun()
 
 def add_bg_from_local():
     st.markdown(
@@ -61,6 +74,7 @@ def main():
     # 앱의 타이틀 설정
     st.title("사랑을 찾아 슝슝~♥")
 
+    # 연애 조언 앱 실행
     app = LoveAdviceApp()
     app.run()
 
